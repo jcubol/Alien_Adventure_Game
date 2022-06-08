688eb8ad8189ce0c13d086e9760bf9f9db501e6b
jcubol <josiah.cubol@gmail.com>
2022-06-08 15:07:53 -0400

    Initial commit

---
+++ TextBasedGame.py
@@ -0,0 +1,12 @@
+#Josiah Cubol
+
+#Welcome to Defeat the Alien
+#Goal: Obtain all 6 items in different rooms throughout the spaceship. You will need ALL 6 items to defeat the Alien or you will lose.
+# Sample movement- 'go North', 'get 'item'
+
+def show_instructions():
+        print("Defeat the Alien Text Adventure Game")
+        print("Collect 6 all 6 items in the spaceship to win the game or be defeated by the invading alien.")
+        print("Move commands: go North, go South, go East, go West")
+        print("Add to Inventory: get 'item name' ")
+
